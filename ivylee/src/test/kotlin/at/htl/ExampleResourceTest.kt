3cea3a141b01aad7385d4ea95e28d727b0ac12cd
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2019-10-04 19:49:31 +0200

    Began Unit Testing

--- ivylee/src/test/kotlin/at/htl/ExampleResourceTest.kt
+++ ivylee/src/test/kotlin/at/htl/ExampleResourceTest.kt
@@ -1,20 +1,26 @@
 package at.htl
 
+import io.quarkus.test.common.QuarkusTestResource
+import io.quarkus.test.h2.H2DatabaseTestResource
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
 import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import javax.inject.Inject
+import javax.persistence.EntityManager
 
 @QuarkusTest
+@QuarkusTestResource(H2DatabaseTestResource::class)
 open class ExampleResourceTest {
 
     @Test
     fun testHelloEndpoint() {
         given()
-          .`when`().get("/hello")
+          .`when`().get("/persons")
           .then()
              .statusCode(200)
-             .body(`is`("hello"))
+             .body("size()", `is`(0))
     }
 
 }
